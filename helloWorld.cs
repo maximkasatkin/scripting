74fbfd2370f7aae2242951b45633d83d6997bd47
maximkasatkin <119167196+maximkasatkin@users.noreply.github.com>
2022-11-27 17:35:39 +0500

    Add files via upload

---
+++ helloWorld.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // helo MNP
+        print("Hello, world");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
